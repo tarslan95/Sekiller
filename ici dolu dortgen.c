858b6fd068d106263737da5dfceca28808a426e2
tarslan95 <isimsizbiradres@hotmail.com>
2014-10-29 02:03:20 +0200

    ici dolu dortgen

--- ici dolu dortgen.c
+++ ici dolu dortgen.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
-
+#include <conio.h>
 
 
 void main()
 {
 
+	int d,y;
+	printf("Dikey * adedini giriniz: ");
+	scanf("%d",&d);
+	printf("Yatay * adedini giriniz: ");
+	scanf("%d",&y);
+
+	for(int i=0;i<d;i++)
+	{
+		for(int j=0;j<y;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 
 
 
-
-
-
-
-
+getch();
 }
