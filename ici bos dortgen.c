c79fb4adbce17d05d996add07a668ef15166baaa
tarslan95 <isimsizbiradres@hotmail.com>
2014-10-29 02:09:30 +0200

    ici bos dortgen.c

--- ici bos dortgen.c
+++ ici bos dortgen.c
@@ -5,6 +5,24 @@
 void main()
 {
 
+	int d,y;
+	printf("Dikey * adedini giriniz: ");
+	scanf("%d",&d);
+	printf("Yatay * adedini giriniz: ");
+	scanf("%d",&y);
+
+	for(int i=0;i<d;i++)
+	{
+		for(int j=0;j<y;j++)
+		{
+			if (i == 0 || j == 0 || i==d-1||j==y-1)
+			printf("*");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+
 
 
 getch();
