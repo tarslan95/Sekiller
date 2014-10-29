4251cb8a0af43acd61ccfc09b3aff2ef022ec579
tarslan95 <isimsizbiradres@hotmail.com>
2014-10-29 09:59:45 +0200

    ucgen1.c

--- ucgen1.c
+++ ucgen1.c
@@ -4,10 +4,32 @@
 
 void main()
 {
+	int kenar;
+	printf("ikiz kenarin yan kenarinin * adedini giriniz: ");
+	scanf("%d",&kenar);
 
-	
-
+	for(int i=1;i<=kenar;i++)
+	{
+		for(int j=0;j<i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 
 
 getch();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
