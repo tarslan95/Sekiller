e2ae82e86172a49ce9ebde245becc754cccac453
tarslan95 <isimsizbiradres@hotmail.com>
2014-10-30 22:02:02 +0200

    ucgen3.c

--- ucgen3.c
+++ ucgen3.c
@@ -1,1 +1,33 @@
-#include 
+#include <stdio.h>
+#include <conio.h>
+
+
+void main()
+{
+	int a;
+	printf("Bir sayi giriniz:");
+	scanf("%d",&a);
+
+	for(int i=0;i<a;i++)
+	{
+		for(int j=a;j>i;j--)
+		{
+			
+			printf("*");
+			
+		}
+		printf("\n");
+
+		for(int b=0;b<i+1;b++)
+		{	
+			printf(" ");
+		}
+		
+		
+		
+		}
+	
+
+
+getch();
+}
